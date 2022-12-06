71069928086444ff55fe67f37ad5a6f368566614
redjen8 <seokwoo2525@gmail.com>
2022-12-07 01:13:07 +0900

    20221207 Fix : UIManager Theme import 문 수정

--- HolubLife/src/com/holub/ui/UIManager.java
+++ HolubLife/src/com/holub/ui/UIManager.java
@@ -5,8 +5,8 @@ import com.holub.life.Cell;
 import com.holub.life.Clock;
 import com.holub.life.SurroundingCells;
 import com.holub.life.SurroundingCells.SurroundingCellsBuilder;
+import com.holub.theme.*;
 import com.holub.life.Universe;
-import com.holub.Theme.*;
 
 import javax.swing.*;
 import java.awt.*;
