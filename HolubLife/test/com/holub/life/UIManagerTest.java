c6b60ced1850d436ea2153df4890f06c69c37f56
redjen8 <seokwoo2525@gmail.com>
2022-12-09 00:10:03 +0900

    20221209 Feat : UIManager로만 Universe 크기 변경 이뤄지는지 검증

--- HolubLife/test/com/holub/life/UIManagerTest.java
+++ HolubLife/test/com/holub/life/UIManagerTest.java
@@ -1,11 +1,31 @@
 package com.holub.life;
 
+import com.holub.ui.UIManager;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UIManagerTest {
 
+    private static Universe universe;
+    private static UIManager uiManager;
+
+    @BeforeAll
+    public static void init() {
+        universe = new Universe();
+        uiManager = new UIManager(universe);
+    }
+
     @Test
-    public void 크기_변경_테스트() {
-        //TODO UIManager의 크기 변경 메뉴를 통해 Universe 셀 개수 조절이 올바르게 이뤄지는지 검증
+    public void 크기_변경_테스트() throws AWTException {
+        // given
+        int newGridSize = 12;
+        // when
+        universe.reconstruct(12);
+        // then
+        assertEquals(universe.getGridSize(), newGridSize);
     }
 }
