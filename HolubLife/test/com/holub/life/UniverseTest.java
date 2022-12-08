25d9c5ea14051f90c12678dc299f4fcd3d26084e
redjen8 <seokwoo2525@gmail.com>
2022-12-09 00:17:09 +0900

    20221209 Feat : Universe clear 메소드 테스트 케이스 추가

--- HolubLife/test/com/holub/life/UniverseTest.java
+++ HolubLife/test/com/holub/life/UniverseTest.java
@@ -1,11 +1,32 @@
 package com.holub.life;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class UniverseTest {
+
+    private static Universe universe;
+
+    @BeforeAll
+    public static void init() {
+        universe = new Universe();
+    }
 
     @Test
     public void 다음_티커_주변_셀_검증() {
         // TODO 다음 틱에 주변 셀들의 상태를 검증
+    }
+
+    @Test
+    public void 클리어_검증() {
+        //given
+
+        //when
+        universe.clear();
+        //then
+        Cell universeCells = universe.getCell();
+        assertFalse(universeCells.isAlive());
     }
 }
