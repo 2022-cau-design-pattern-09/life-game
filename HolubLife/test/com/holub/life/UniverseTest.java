0a91049e4f9c4e3fcf004b3edc36eb9763deed0f
redjen8 <seokwoo2525@gmail.com>
2022-12-08 23:44:08 +0900

    20221208 Feat : 현재 진행 상태에 관한 UniverseTest 추가

---
+++ HolubLife/test/com/holub/life/UniverseTest.java
@@ -0,0 +1,11 @@
+package com.holub.life;
+
+import org.junit.jupiter.api.Test;
+
+public class UniverseTest {
+
+    @Test
+    public void 다음_티커_주변_셀_검증() {
+        // TODO 다음 틱에 주변 셀들의 상태를 검증
+    }
+}
