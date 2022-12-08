4efd7a3a0721eb664e7025b8d479cf28020ef74f
redjen8 <seokwoo2525@gmail.com>
2022-12-08 23:44:36 +0900

    20221208 Feat : JFrame UI에서 크기 변경이 잘 이뤄지는지 검증하는 테스트 추가

---
+++ HolubLife/test/com/holub/life/UIManagerTest.java
@@ -0,0 +1,11 @@
+package com.holub.life;
+
+import org.junit.jupiter.api.Test;
+
+public class UIManagerTest {
+
+    @Test
+    public void 크기_변경_테스트() {
+        //TODO UIManager의 크기 변경 메뉴를 통해 Universe 셀 개수 조절이 올바르게 이뤄지는지 검증
+    }
+}
