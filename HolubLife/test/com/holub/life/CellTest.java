df1d2a5d60b82fd7a3393872f79ec8e01b7785b8
SuperChobo <hjk8209@naver.com>
2022-12-09 22:49:15 +0900

    feat: 테스트 케이스 추가

--- HolubLife/test/com/holub/life/CellTest.java
+++ HolubLife/test/com/holub/life/CellTest.java
@@ -1,15 +1,12 @@
 package com.holub.life;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 
-import com.holub.rule.*;
+import com.holub.rule.RelativePosition;
+import com.holub.rule.Rule;
 
 public class CellTest {
     private static Universe universe;
@@ -20,101 +17,8 @@ public class CellTest {
         universe = new Universe();
     }
 
-    @Test
-    public void testOfTestMethod1() {
-        //given
-        Cell grid = makeGrid(3);
-        //when
-        setAlive(grid.at(0, 0));
-        //then
-        assertTrue(grid.at(0, 0).isAlive());
-    }
+    
 
-    @Test
-    public void testOfTestMethod2() {
-        //given
-        int gridSize = 3;
-        boolean[][] alive = {
-                            {true, true, false}, 
-                            {true, true, false}, 
-                            {true, true, false}
-                        };
-        Cell grid = makeGrid(gridSize, alive);
-
-        //when
-        setAlive(grid.at(0, 0));
-
-        //then
-        assertTrue(grid.at(0, 0).isAlive());
-        assertTrue(grid.at(0, 1).isAlive());
-        assertFalse(grid.at(0, 2).isAlive());
-        assertTrue(grid.at(1, 0).isAlive());
-        assertTrue(grid.at(1, 1).isAlive());
-        assertFalse(grid.at(1, 2).isAlive());
-        assertTrue(grid.at(2, 0).isAlive());
-        assertTrue(grid.at(2, 1).isAlive());
-        assertFalse(grid.at(2, 2).isAlive());
-    }
-
-    @Test
-    public void OriginalRuleTest_1() {
-        // given
-        int gridSize = 3;
-        boolean[][] alive = {
-                            {true, true, false}, 
-                            {true, true, false}, 
-                            {false, false, false}
-                        };
-        Rule rule = new OriginalRule();
-        Cell grid = makeGrid(gridSize, alive);
-        List<Cell> neighbors = makeNeighbors(grid, 1, 1, rule);
-
-        //when
-        boolean willBeAlive = rule.willBeAlive(neighbors, true);
-
-        //then
-        assertTrue(willBeAlive);
-    }
-
-    @Test
-    public void OriginalRuleTest_2() {
-        // given
-        int gridSize = 3;
-        boolean[][] alive = {
-                            {true, true, false}, 
-                            {true, true, false}, 
-                            {false, false, true}
-                        };
-        Rule rule = new OriginalRule();
-        Cell grid = makeGrid(gridSize, alive);
-        List<Cell> neighbors = makeNeighbors(grid, 1, 1, rule);
-
-        //when
-        boolean willBeAlive = rule.willBeAlive(neighbors, true);
-
-        //then
-        assertFalse(willBeAlive);
-    }
-
-    @Test
-    public void OriginalRuleTest_3() {
-        // given
-        int gridSize = 3;
-        boolean[][] alive = {
-                            {true, false, true}, 
-                            {false, true, false}, 
-                            {false, false, false}
-                        };
-        Rule rule = new OriginalRule();
-        Cell grid = makeGrid(gridSize, alive);
-        List<Cell> neighbors = makeNeighbors(grid, 1, 1, rule);
-
-        //when
-        boolean willBeAlive = rule.willBeAlive(neighbors, true);
-
-        //then
-        assertTrue(willBeAlive);
-    }
 
     // 
     // Methods for Test
--- HolubLife/test/com/holub/life/UniverseTest.java
+++ HolubLife/test/com/holub/life/UniverseTest.java
@@ -1,9 +1,18 @@
 package com.holub.life;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.holub.life.SurroundingCells.SurroundingCellsBuilder;
+import com.holub.rule.BigDiamondRule;
+import com.holub.rule.OriginalRule;
+import com.holub.rule.Rule;
 
 public class UniverseTest {
 
@@ -12,11 +21,6 @@ public class UniverseTest {
     @BeforeAll
     public static void init() {
         universe = new Universe();
-    }
-
-    @Test
-    public void nextTickTest() {
-        // TODO 다음 틱에 주변 셀들의 상태를 검증
     }
 
     @Test
@@ -30,10 +34,22 @@ public class UniverseTest {
     }
 
     @Test
+    public void clickedTwiceTest(){
+        //given
+        Cell target = universe.getCell().at(0, 0).at(0, 0);
+        target.userClicked(null, null); // When Target is Alive
+
+        //when
+        target.userClicked(null, null);
+        //then
+        assertFalse(target.isAlive());    
+    }
+
+    @Test
     public void clearTest() {
         //given
         Cell target = universe.getCell().at(0, 0).at(0, 0);
-        target.userClicked(null, null);
+        target.userClicked(null, null); // Target is Alive
         //when
         universe.clear();
         //then
@@ -49,4 +65,135 @@ public class UniverseTest {
         // then
         assertEquals(universe.getGridSize(), newGridSize);
     }
+
+    @Test
+    public void setRuleTest(){
+        // given
+        Rule rule = new OriginalRule();
+        // when
+        universe.setRule(rule);
+        //then
+        assertTrue(universe.getRule() instanceof OriginalRule);
+    }
+
+    @Test
+    public void ruleChangeTest(){
+        // given
+        Rule rule = new BigDiamondRule();
+        universe.setRule(new OriginalRule());
+
+        // when
+        universe.setRule(rule);
+
+        // then
+        assertAll(
+            () -> assertTrue(universe.getRule() instanceof BigDiamondRule),
+            () -> assertFalse(universe.getRule() instanceof OriginalRule)
+        );
+    }
+
+    @DisplayName("3x3으로 활성화 되어있을 때, 살아있는 셀들 테스트")
+    @Test
+    public void nextTickTest_1() {
+        Universe universe = new Universe();
+        //given
+        universe.setRule(new OriginalRule());
+        universe.reconstruct(3);
+        Cell grid = universe.getCell().at(0, 0);
+        setAlive(grid.at(0, 0));
+        setAlive(grid.at(0, 1));
+        setAlive(grid.at(0, 2));
+        setAlive(grid.at(1, 0));
+        setAlive(grid.at(1, 1));
+        setAlive(grid.at(1, 2));
+        setAlive(grid.at(2, 0));
+        setAlive(grid.at(2, 1));
+        setAlive(grid.at(2, 2));
+
+        SurroundingCells dummySurroundingCells = new SurroundingCellsBuilder()
+            .setNorthWest(Cell.DUMMY)
+            .setNorthEast(Cell.DUMMY)
+            .setNorth(Cell.DUMMY)
+            .setSouthWest(Cell.DUMMY)
+            .setSouthEast(Cell.DUMMY)
+            .setSouth(Cell.DUMMY)
+            .setWest(Cell.DUMMY)
+            .setEast(Cell.DUMMY)
+            .build();
+
+        //when
+
+        universe.getCell().figureNextState(dummySurroundingCells, universe.getRule());
+        universe.getCell().transition();
+        boolean check[][] = new boolean[3][3];
+        for(int row = 0; row < 3; row++){
+            for(int column = 0; column < 3; column++){
+                check[row][column] = grid.at(row, column).isAlive();
+            }
+        }
+
+        //then
+        assertAll(
+            () -> assertTrue(check[0][0]),
+            () -> assertFalse(check[0][1]),
+            () -> assertTrue(check[0][2]),
+            () -> assertFalse(check[1][0]),
+            () -> assertFalse(check[1][1]),
+            () -> assertFalse(check[1][2]),
+            () -> assertTrue(check[2][0]),
+            () -> assertFalse(check[2][1]),
+            () -> assertTrue(check[2][2])
+        );
+    }
+
+    @DisplayName("주변에 활성화 된 셀이 3개일 때 유지되는 셀 테스트")
+    @Test
+    public void nextTickTest_2() {
+        //given
+        universe.setRule(new OriginalRule());
+        Cell grid = universe.getCell().at(0, 0);
+
+        setAlive(grid.at(0, 0));
+        setAlive(grid.at(1, 0)); 
+        setAlive(grid.at(2, 0));
+
+        setAlive(grid.at(1, 1));
+
+        SurroundingCells dummySurroundingCells = new SurroundingCellsBuilder()
+            .setNorthWest(Cell.DUMMY)
+            .setNorthEast(Cell.DUMMY)
+            .setNorth(Cell.DUMMY)
+            .setSouthWest(Cell.DUMMY)
+            .setSouthEast(Cell.DUMMY)
+            .setSouth(Cell.DUMMY)
+            .setWest(Cell.DUMMY)
+            .setEast(Cell.DUMMY)
+            .build();
+
+        //when
+        universe.getCell().figureNextState(dummySurroundingCells, universe.getRule());
+        universe.getCell().transition();
+
+        //then
+        assertFalse(grid.at(0, 0).isAlive());
+        assertFalse(grid.at(0, 1).isAlive());
+        assertFalse(grid.at(0, 2).isAlive());
+        assertFalse(grid.at(1, 0).isAlive());
+        assertFalse(grid.at(1, 1).isAlive());
+        assertFalse(grid.at(1, 2).isAlive());
+        assertFalse(grid.at(2, 0).isAlive());
+        assertFalse(grid.at(2, 1).isAlive());
+        assertFalse(grid.at(2, 2).isAlive());
+    }
+
+    // 
+    // Methods for Test
+    // 
+
+    private static void setAlive(Cell cell){
+        if(!cell.isAlive()){
+            cell.userClicked(null, null);
+        }
+    }
 }
+
