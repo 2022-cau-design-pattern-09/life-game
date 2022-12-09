3502b3c9f4dea463768ee754bfe0085619fea510
SuperChobo <hjk8209@naver.com>
2022-12-09 23:57:22 +0900

    Refactor: universe 관련 테스트 케이스 추가

--- HolubLife/test/com/holub/life/UniverseTest.java
+++ HolubLife/test/com/holub/life/UniverseTest.java
@@ -95,11 +95,10 @@ public class UniverseTest {
     @DisplayName("3x3으로 활성화 되어있을 때, 살아있는 셀들 테스트")
     @Test
     public void nextTickTest_1() {
-        Universe universe = new Universe();
         //given
         universe.setRule(new OriginalRule());
-        universe.reconstruct(3);
         Cell grid = universe.getCell().at(0, 0);
+
         setAlive(grid.at(0, 0));
         setAlive(grid.at(0, 1));
         setAlive(grid.at(0, 2));
@@ -146,18 +145,51 @@ public class UniverseTest {
         );
     }
 
-    @DisplayName("주변에 활성화 된 셀이 3개일 때 유지되는 셀 테스트")
+    @DisplayName("주변에 활성화 된 셀이 3개일 때 새로 생성되는 셀 테스트")
     @Test
     public void nextTickTest_2() {
         //given
         universe.setRule(new OriginalRule());
         Cell grid = universe.getCell().at(0, 0);
-
+        
         setAlive(grid.at(0, 0));
-        setAlive(grid.at(1, 0)); 
-        setAlive(grid.at(2, 0));
+        setAlive(grid.at(1, 2));
+        setAlive(grid.at(2, 2));
 
-        setAlive(grid.at(1, 1));
+        SurroundingCells dummySurroundingCells = new SurroundingCellsBuilder()
+            .setNorthWest(Cell.DUMMY)
+            .setNorthEast(Cell.DUMMY)
+            .setNorth(Cell.DUMMY)
+            .setSouthWest(Cell.DUMMY)
+            .setSouthEast(Cell.DUMMY)
+            .setSouth(Cell.DUMMY)
+            .setWest(Cell.DUMMY)
+            .setEast(Cell.DUMMY)
+            .build();
+
+        //when
+
+        universe.getCell().figureNextState(dummySurroundingCells, universe.getRule());
+        universe.getCell().transition();
+        boolean check = grid.at(1, 1).isAlive();
+
+        //then
+        assertTrue(check);
+    }
+
+    @DisplayName("작은 그리드의 경계선에서 잘 작동하는지에 대한 테스트")
+    @Test
+    public void nextTickTest_3() {
+        //given
+        universe.setRule(new OriginalRule());
+        universe.reconstruct(3);
+        
+        Cell grid1 = universe.getCell().at(0, 0);
+        Cell grid2 = universe.getCell().at(0, 1);
+
+        setAlive(grid1.at(0, 1));
+        setAlive(grid2.at(0, 0));
+        setAlive(grid2.at(1, 0));
 
         SurroundingCells dummySurroundingCells = new SurroundingCellsBuilder()
             .setNorthWest(Cell.DUMMY)
@@ -173,17 +205,22 @@ public class UniverseTest {
         //when
         universe.getCell().figureNextState(dummySurroundingCells, universe.getRule());
         universe.getCell().transition();
+        boolean check[] = {
+            grid1.at(0, 1).isAlive(),
+            grid1.at(0, 2).isAlive(),
+            grid1.at(1, 2).isAlive(),
+            grid2.at(0, 0).isAlive(),
+            grid2.at(1, 0).isAlive()
+        };
 
         //then
-        assertFalse(grid.at(0, 0).isAlive());
-        assertFalse(grid.at(0, 1).isAlive());
-        assertFalse(grid.at(0, 2).isAlive());
-        assertFalse(grid.at(1, 0).isAlive());
-        assertFalse(grid.at(1, 1).isAlive());
-        assertFalse(grid.at(1, 2).isAlive());
-        assertFalse(grid.at(2, 0).isAlive());
-        assertFalse(grid.at(2, 1).isAlive());
-        assertFalse(grid.at(2, 2).isAlive());
+        assertAll(
+            () -> assertFalse(check[0]),
+            () -> assertTrue(check[1]),
+            () -> assertTrue(check[2]),
+            () -> assertFalse(check[3]),
+            () -> assertFalse(check[4])
+        );
     }
 
     // 
